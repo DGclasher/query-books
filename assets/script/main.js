dffb3580b07f0963b474b4f1b93f582d94df6bd2
DGclasher <73327721+DGclasher@users.noreply.github.com>
2023-01-12 03:51:11 +0530

    initial stage

---
+++ assets/script/main.js
@@ -0,0 +1,40 @@
+
+
+let getBooks = {
+    fetchKey : function(query){
+        fetch("https://openlibrary.org/search.json?q="+query)
+        .then((response)=>response.json())
+        .then((response)=>this.getKey(response.docs))
+
+    },
+
+    getKey : function(data){
+        const { key } = data[0];
+        console.log(key)
+        this.fetchBook(key);
+    },
+
+    fetchBook : function(key){
+        fetch("https://openlibrary.org"+key+".json")
+        .then(response=>response.json())
+        .then(response=>this.displayBook(response))
+    },
+
+    displayBook : function(data){
+        const {description, title, covers} = data;
+        console.log(title)
+        document.getElementById("book_title").innerHTML=title;
+        document.getElementById("book_desc").innerHTML=description
+        document.getElementById("book_cover").setAttribute('src', "https://covers.openlibrary.org/b/id/"+covers[0]+".jpg")
+    }
+
+}
+
+// https://covers.openlibrary.org/b/isbn/9788533619623.jpg -> Cover
+// https://covers.openlibrary.org/b/id/9255566.jpg
+
+document.getElementById("search_btn").addEventListener("click", function(){
+    let query = document.getElementById("book_query").value
+    console.log(query)
+    getBooks.fetchKey(query);
+})
